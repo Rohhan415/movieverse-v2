e43e9e700239dfdcf32802f6cfcaa9f113f8df38
Kacper <rohhan.514@gmail.com>
2024-08-01 22:42:13 +0200

    feature: improving slider arrows

--- app/_components/customArrows.tsx
+++ app/_components/customArrows.tsx
@@ -17,15 +17,15 @@ export function NextArrow(props: ArrowProps) {
       <svg
         data-slot="icon"
         fill="none"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="m8.25 4.5 7.5 7.5-7.5 7.5"
         ></path>
       </svg>
