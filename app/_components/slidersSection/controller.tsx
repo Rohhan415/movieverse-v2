4afc9dbcbe8943a3eb89015c705136b078e1d881
Kacper <rohhan.514@gmail.com>
2024-08-02 22:56:13 +0200

    fix: added controller.tsx

---
+++ app/_components/slidersSection/controller.tsx
@@ -0,0 +1,58 @@
+// sliderSettings.ts
+import {
+  NextArrow,
+  PrevArrow,
+} from "@/app/_components/slidersSection/customArrows";
+
+export const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  centerMode: true,
+  className: "display: flex; justify-content: center;",
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+  responsive: [
+    {
+      breakpoint: 1680,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1320,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
